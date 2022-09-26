3cd848e501876651c956cb91caa1b601f296da6d
Adam Smallman <adam.smallman@3gtelecoms.net>
2022-09-27 00:09:57 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,52 +2,8 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [tempValue, setTempValue] = useState(0);
-  const [tempColor, setTempColor] = useState('cold');
-
-
-
-  const inTemp = () => {
-
-    if (tempValue === 30) return;
-
-    if (tempValue < 14) {
-
-      setTempColor('cold')
-
-
-    } else {
-
-      setTempColor('hot')
-
-      console.log(tempValue)
-
-    }
-
-    setTempValue(tempValue + 1)
-
-  }
-
-
-  const dTemp = () => {
-
-    if (tempValue === 0) return;
-
-
-    if (tempValue > 15) {
-
-      setTempColor('hot')
-
-    } else {
-
-      setTempColor('cold')
-
-    }
-
-    setTempValue(tempValue - 1)
-
-  }
-
+  const [tempValue, setTempValue] = useState(10);
+  
 
   return (
     <div className="App">
@@ -56,14 +12,14 @@ function App() {
 
         <div className='temperature-display-container'>
 
-          <div className={`temperature-display ${tempColor}`}>{tempValue}°C</div>
+          <div className={`temperature-display ${tempValue < 15 ? "cold" : "hot"}`}>{tempValue}°C</div>
 
         </div>
 
         <div className='button-container'>
 
-          <button onClick={inTemp}>+</button>
-          <button onClick={dTemp}>-</button>
+          <button onClick={()=>setTempValue(tempValue + 1)} disabled={tempValue >= 30 }>+</button>
+          <button onClick={()=>setTempValue(tempValue - 1)} disabled={tempValue <= 0 }>-</button>
 
         </div>
 
